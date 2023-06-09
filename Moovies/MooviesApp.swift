1d406d19543084d639688d51c877b65b4153852b
Thant Sin Htun <thantsinhtun@aceplussolutions.com>
2023-06-09 09:48:03 +0630

    Initial Commit

---
+++ Moovies/MooviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MooviesApp.swift
+//  Moovies
+//
+//  Created by Thant Sin Htun on 09/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MooviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
